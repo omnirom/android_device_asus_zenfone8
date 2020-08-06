dd7ffab8d3987857e1991a60ea7a2846c5f90af1
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-06 13:41:45 +0200

    zenfone8: Initial FOD hooks
    Change-Id: I89a65e20e07493ba1e87f116f6178964d7af59a0

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -27,27 +27,43 @@ namespace inscreen {
 namespace V1_0 {
 namespace implementation {
 
+FingerprintInscreen::FingerprintInscreen() {
+    this->mGoodixFingerprintDaemon = IGoodixFingerprintDaemon::getService();
+}
+
 Return<void> FingerprintInscreen::onStartEnroll() {
     return Void();
 }
 
 Return<void> FingerprintInscreen::onFinishEnroll() {
+    this->mGoodixFingerprintDaemon->sendCommand(200000, {},
+                                                [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    this->mGoodixFingerprintDaemon->sendCommand(200001, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+    this->mGoodixFingerprintDaemon->sendCommand(200002, {},
+                                                [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    this->mGoodixFingerprintDaemon->sendCommand(200003, {},
+                                                [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    this->mGoodixFingerprintDaemon->sendCommand(200002, {},
+                                                [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    this->mGoodixFingerprintDaemon->sendCommand(200000, {},
+                                                [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
