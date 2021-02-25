68dd7df06068cd7691dff4d457c7d079675a5d08
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-24 20:56:15 -0500

    zenfone8: Drop PRODUCT_PACKAGES_DEBUG entry
    * unnecessary
    
    Change-Id: I22ae2c744ec55168b8367c826e55110e7534466e

--- device.mk
+++ device.mk
@@ -116,9 +116,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Charger images
 PRODUCT_PACKAGES += \
     omni_charger_res_images \
@@ -291,9 +288,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_HOST_PACKAGES += \
     brillo_update_payload
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
