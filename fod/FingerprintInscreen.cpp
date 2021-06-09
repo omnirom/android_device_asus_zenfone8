5b645f598c128766923d5911d912e6e512940b2a
micky387 <mickaelsaibi@free.fr>
2021-06-09 02:53:40 +0200

    zenfone8: Add FOD size and position
    Change-Id: I114b73f8b2db16457114c8c8c1d1b6bb60f0b89a

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -92,15 +92,15 @@ Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallb
 }
 
 Return<int32_t> FingerprintInscreen::getPositionX() {
-    return 0;
+    return 410;
 }
 
 Return<int32_t> FingerprintInscreen::getPositionY() {
-    return 0;
+    return 1761;
 }
 
 Return<int32_t> FingerprintInscreen::getSize() {
-    return 0;
+    return 310;
 }
 
 }  // namespace implementation
