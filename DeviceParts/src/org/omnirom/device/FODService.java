9e87a2bbd52327371661a41cb38b751819c4e134
micky387 <mickaelsaibi@free.fr>
2021-07-07 13:14:50 +0200

    zenfone8: Enable/Disable FOD on demande
    Change-Id: I170667b89e3c7518b9433758089c89af63934b42

---
+++ DeviceParts/src/org/omnirom/device/FODService.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2021 The OmniRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.omnirom.device;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.fingerprint.IFingerprintService;
+import android.os.IBinder;
+import android.os.Messenger;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.util.Log;
+
+public class FODService extends Service {
+    private Context mContext;
+    private IFingerprintService mFingerprintService;
+    private static final String TAG = "FODService";
+    private static final boolean DEBUG = true;
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        registerReceiver();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        this.unregisterReceiver(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        this.registerReceiver(this, filter);
+        mFingerprintService = IFingerprintService.Stub.asInterface(
+                                ServiceManager.getService(Context.FINGERPRINT_SERVICE));
+        updateFingerprint();
+    }
+
+    private void updateFingerprint() {
+        boolean isFPClientActive = false;
+        String value = SystemProperties.get("persist.vendor.asus.fp.wakeup");
+        try {
+            isFPClientActive = mFingerprintService.isClientActive();
+        } catch (Exception e) {
+            // do nothing.
+        }
+        if (!isFPClientActive) {
+            SystemProperties.set("persist.vendor.asus.fp.wakeup", "false");
+        } else {
+            SystemProperties.set("persist.vendor.asus.fp.wakeup", "true");
+        }
+        if (DEBUG) Log.i(TAG, "Finerprint wakepup = " + value);
+    }
+}
