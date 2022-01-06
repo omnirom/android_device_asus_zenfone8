20b459d4dfb94f243bbe8d550ee25f345299ee8b
micky387 <mickaelsaibi@free.fr>
2022-01-06 23:12:42 +0100

    zenfone8: Build the new qti telephony packages and add extphonelib to telephony_injection
    Change-Id: Iadab25ab352e9a0a083bd507393f4ee54f35eae7

--- device.mk
+++ device.mk
@@ -204,12 +204,20 @@ PRODUCT_PACKAGES += \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    qti-telephony-utils-prd \
+    qti_telephony_utils_prd.xml \
     tcmiface
 
 # Update engine
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
