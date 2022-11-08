7ea8cf4ea3980f2311d964e251baa70c4065dd3e
maxwen <max.weninger@gmail.com>
2022-11-08 22:37:36 +0100

    DeviceParts: switch to Material3
    Change-Id: If8a127bc29f33f9901b1657ad75c91c2b6ac1fba

--- DeviceParts/src/org/omnirom/device/DozeSettings.java
+++ DeviceParts/src/org/omnirom/device/DozeSettings.java
@@ -22,7 +22,7 @@ import android.app.Dialog;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -37,7 +37,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.util.Log;
 
-public class DozeSettings extends PreferenceFragment  {
+public class DozeSettings extends PreferenceFragmentCompat {
 
     private static final String KEY_WAVE_CHECK = "wave_check";
     private static final String KEY_POCKET_CHECK = "pocket_check";
