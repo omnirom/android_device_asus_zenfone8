658fb8c4d031d291e56aea96093f16b55f3ff95a
micky387 <mickaelsaibi@free.fr>
2023-05-26 22:00:02 +0200

    zenfone8: Add Asus AudioWiardView and Dirac service
    Squashed with this following commit
    
    commit 0d732a67dcf8030b81580de0e955585db682c124
    Author: Alexander Koskovich <akoskovich@pm.me>
    Date:   Wed Mar 9 01:43:27 2022 -0700
    
        zenfone8: sepolicy: Add Dirac audio properties & policy
    
        Change-Id: I874c626f9565bcbf465149804455ce290be1c3d0
    
    [micky387]
    remove Google AD_ID permission for diracaudio bootreceiver since theirs no ADS.
    
    W AdvertisingIdSettings: Package org.omnirom.diracaudio failed Ad Id permission check. Apps that target Android SDK 33 or higher should declare com.google.android.gms.permission.AD_ID in the app manifest to access Ad Id.
    
    Change-Id: I22cd02fa271966fbd5d5c6f83b2a5c100f967ce6

---
+++ DeviceParts/src/org/omnirom/device/AudioSettingsActivity.java
@@ -0,0 +1,65 @@
+/*
+* Copyright (C) 2023 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AudioSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.settings_activity);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, new AudioSettings())
+            .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
