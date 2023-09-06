993f0d6b339d365a921ddf1a5fe4976263396913
micky387 <mickaelsaibi@free.fr>
2023-09-06 23:58:50 +0200

    zenfone8: Update partitions RESERVED_SIZE for NikGapps packages
    Change-Id: Ibdbfc3d4b70fb2c9c4e695cb95ae53372db654f5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 # device-specific aspects (drivers) with a device-agnostic
 # product configuration (apps).
 #
-DEVICE_PATH := device/asus/zenfone8
+DEVICE_PATH := device/asus/zenfone9
 
 BOARD_VENDOR := asus
 
@@ -30,42 +30,31 @@ AB_OTA_PARTITIONS += \
     dtbo \
     odm \
     product \
+    recovery \
     system \
     system_ext \
     vbmeta \
     vbmeta_system \
     vendor \
-    vendor_boot
+    vendor_boot \
+    vendor_dlkm
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-a-branchprot
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := kryo385
+TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
-
-# Audio
-AUDIO_FEATURE_ENABLED_DLKM := true
-AUDIO_FEATURE_ENABLED_GKI := true
-AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
-AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+TARGET_2ND_CPU_VARIANT := cortex-a76
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
-# Biometrics
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.sake
-
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
 
 # A/B
@@ -78,11 +67,16 @@ BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Audio
-#USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
 AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
@@ -94,7 +88,9 @@ AUDIO_FEATURE_ENABLED_VOICE_CONCURRENCY := true
 # Broken flags
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
+
+# Camera
+TARGET_CAMERA_NEEDS_CLIENT_INFO := true
 
 # charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
@@ -107,9 +103,9 @@ ifeq ($(HOST_OS),linux)
 endif
 
 # Display
-TARGET_USES_HWC2 := true
 TARGET_SCREEN_DENSITY := 440
-SOONG_CONFIG_qtidisplay_udfps := true
+SOONG_CONFIG_qtidisplay_displayconfig_enabled := true
+SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
@@ -129,138 +125,51 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/manifest/vendor_framework_compatibility_matrix.xml \
                                               $(DEVICE_PATH)/manifest/asus_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/manifest/framework_manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest/manifest_lahaina.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest/manifest_cape.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/manifest/compatibility_matrix.xml
 
 # Kernel
-KERNEL_LD := LD=ld.lld AR=llvm-ar
-BOARD_BOOT_HEADER_VERSION := 3
+BOARD_USES_DT := true
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_NO_KERNEL := false
+TARGET_NO_KERNEL_OVERRIDE := true
+
+BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 firmware_class.path=/vendor/firmware loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE := \
+    msm_geni_serial.con_enabled=0
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3
+
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE := 0x00000000
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := vendor/zf8_defconfig
-TARGET_KERNEL_SOURCE := kernel/asus/sm8350
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
-BOARD_VENDOR_KERNEL_MODULES_LOAD := \
-    adsp_loader_dlkm.ko \
-    apr_dlkm.ko \
-    aw8697.ko \
-    bolero_cdc_dlkm.ko \
-    bt_fm_slim.ko \
-    btpower.ko \
-    camera.ko \
-    cs35l45_i2c_dlkm.ko \
-    e4000.ko \
-    fc0011.ko \
-    fc0012.ko \
-    fc0013.ko \
-    fc2580.ko \
-    focaltech_fts_zf.ko \
-    gf_spi.ko \
-    hdmi_dlkm.ko \
-    hid-aksys.ko \
-    icm206xx.ko \
-    it913x.ko \
-    lid.ko \
-    lid_2.ko \
-    llcc_perfmon.ko \
-    m88rs6000t.ko \
-    machine_dlkm.ko \
-    max2165.ko \
-    mbhc_dlkm.ko \
-    mc44s803.ko \
-    msi001.ko \
-    msm_drm.ko \
-    mt2060.ko \
-    mt2063.ko \
-    mt20xx.ko \
-    mt2131.ko \
-    mt2266.ko \
-    mxl301rf.ko \
-    mxl5005s.ko \
-    mxl5007t.ko \
-    native_dlkm.ko \
-    pinctrl_lpi_dlkm.ko \
-    pinctrl_wcd_dlkm.ko \
-    platform_dlkm.ko \
-    q6_dlkm.ko \
-    q6_notifier_dlkm.ko \
-    q6_pdr_dlkm.ko \
-    wlan.ko \
-    qcom_edac.ko \
-    qm1d1b0004.ko \
-    qm1d1c0042.ko \
-    qt1010.ko \
-    r820t.ko \
-    rdbg.ko \
-    rmnet_core.ko \
-    rmnet_ctl.ko \
-    rmnet_offload.ko \
-    rmnet_shs.ko \
-    rx_macro_dlkm.ko \
-    sensors_vcnl36866.ko \
-    si2157.ko \
-    sla.ko \
-    slimbus-ngd.ko \
-    slimbus.ko \
-    snd-soc-es928x.ko \
-    snd_event_dlkm.ko \
-    stub_dlkm.ko \
-    swr_ctrl_dlkm.ko \
-    swr_dlkm.ko \
-    swr_dmic_dlkm.ko \
-    swr_haptics_dlkm.ko \
-    sx932x_sake.ko \
-    tda18212.ko \
-    tda18218.ko \
-    tda18250.ko \
-    tda9887.ko \
-    tea5761.ko \
-    tea5767.ko \
-    tua9001.ko \
-    tuner-simple.ko \
-    tuner-types.ko \
-    tuner-xc2028.ko \
-    tx_macro_dlkm.ko \
-    va_macro_dlkm.ko \
-    wcd937x_dlkm.ko \
-    wcd937x_slave_dlkm.ko \
-    wcd938x_dlkm.ko \
-    wcd938x_slave_dlkm.ko \
-    wcd9xxx_dlkm.ko \
-    wcd_core_dlkm.ko \
-    wsa883x_dlkm.ko \
-    wsa_macro_dlkm.ko \
-    xc4000.ko \
-    xc5000.ko
-
-BOOT_KERNEL_MODULES := \
-    msm_drm.ko
-
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := \
-    msm_drm.ko
+# Kernel Modules
+KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)/modules
+KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(KERNEL_MODULE_DIR)/vendor_dlkm.modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/vendor_dlkm.modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
 
 # NFC
 TARGET_USES_NQ_NFC := true
 
 # Partitions
 ifneq ($(ROM_BUILDTYPE),$(filter $(ROM_BUILDTYPE),GAPPS MICROG))
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1258291200
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1258291200
-BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 629145600
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1629145600
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 429145600
+BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 429145600
 endif
 
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -269,35 +178,39 @@ BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_KERNEL_BINARIES := kernel
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 495457218560
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 234211844096
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_USES_METADATA_PARTITION := true
 TARGET_COPY_OUT_ODM := odm
 TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     system \
     system_ext \
     odm \
     product \
-    vendor
+    vendor \
+    vendor_dlkm
 
-BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 7512192768 # BOARD_SUPER_PARTITION_SIZE - 4MB
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # BOARD_SUPER_PARTITION_SIZE - 4MB (4194304)
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
-BOARD_SUPER_PARTITION_SIZE := 7516192768
+BOARD_SUPER_PARTITION_SIZE := 9126805504
 
 BOARD_ROOT_EXTRA_FOLDERS += ADF APD asdf batinfo
 
 # Platform
-QCOM_HARDWARE_VARIANT := sm8350
-TARGET_BOARD_PLATFORM := lahaina
+QCOM_HARDWARE_VARIANT := sm8450
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno660
+TARGET_USES_KERNEL_PLATFORM := true
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
@@ -306,34 +219,46 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-BOARD_USES_RECOVERY_AS_BOOT := true
-TARGET_NO_RECOVERY := true
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/ramdisk/fstab.default
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/ramdisk/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 150
+BOARD_USES_FULL_RECOVERY_IMAGE := true
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(KERNEL_MODULE_DIR)/vendor_dlkm.modules.blocklist
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/vendor_boot.modules.load))
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-03-05
+BOOT_SECURITY_PATCH := 2022-12-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
 include vendor/omni/sepolicy/sepolicy.mk
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vendor/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+
+# VNDK
+NEED_AIDL_NDK_PLATFORM_BACKEND := true
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_PRIVATE_LIB_EVENT := "ON"
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB_EVENT := "ON"
 PRODUCT_VENDOR_MOVE_ENABLED := true
 WIFI_DRIVER_DEFAULT := qca_cld3
 WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wlan"
 WIFI_DRIVER_STATE_OFF := "OFF"
 WIFI_DRIVER_STATE_ON := "ON"
+WIFI_HIDL_FEATURE_AWARE := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
