1409066534a9dada5a1b179f6a2ed939e2c512f5
micky387 <mickaelsaibi@free.fr>
2023-10-15 14:54:30 +0200

    zenfone8: Build android.frameworks.sensorservice@1.0 on vendor
    E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: library "android.frameworks.sensorservice@1.0.so" not found: needed by /vendor/lib64/hw/camera.qcom.so in namespace (default).
    
    Change-Id: I56fd07b85c494fc1af7f7991ab523d3ffbd9a890

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor
+
 # Charger images
 PRODUCT_PACKAGES += \
     omni_charger_res_images \
