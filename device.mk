d30b047cdc5875b1052e5327002eeefac332e43f
micky387 <mickaelsaibi@free.fr>
2024-01-21 08:21:25 +0100

    zenfone8: Replace call inherit-product by include Makefile
    This makefiles are called before all the deps are sync with omni.dependencies and cause build stop in some cases.
    
    Change-Id: I3aa286e9cd19e86a1a0de3b8fb79bf5684554a36

--- device.mk
+++ device.mk
@@ -153,13 +153,13 @@ PRODUCT_PACKAGES += \
     libtinyxml2 \
     lights.qcom
 
-$(call inherit-product, hardware/qcom-caf/sm8350/media/product.mk)
-$(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-board.mk)
-$(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.mk)
-$(call inherit-product, vendor/qcom/opensource/display/config/display-product-vendor.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys/display_custom/config/display-product-commonsys.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
+-include hardware/qcom-caf/sm8350/media/product.mk
+-include hardware/qcom-caf/sm8350/display/config/display-board.mk
+-include hardware/qcom-caf/sm8350/display/config/display-product.mk
+-include vendor/qcom/opensource/display/config/display-product-vendor.mk
+-include vendor/qcom/opensource/commonsys/display_custom/config/display-product-commonsys.mk
+-include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
+-include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
 
 PRODUCT_PACKAGES += \
     libtinyalsa
