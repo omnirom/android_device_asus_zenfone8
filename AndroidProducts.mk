6bf2687d8a0f93ca8b58b2158c0f641ede5e3e24
micky387 <mickaelsaibi@free.fr>
2024-03-18 21:34:31 -0400

    zenfone8: Fix COMMON_LUNCH_CHOICES release
    trunk_staging is not shipped by AOSP yet . Only ap1a is available but it's really bad to hardcoded this release version.
    Not thx Google.... :(
    
    Change-Id: Ib89dbffaa9a2ae0db6f3ac58920b12e5fca71520

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_zenfone8.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_zenfone8-user \
-    omni_zenfone8-trunk_staging-userdebug \
-    omni_zenfone8-eng
+    omni_zenfone8-ap1a-user \
+    omni_zenfone8-ap1a-userdebug \
+    omni_zenfone8-ap1a-eng
