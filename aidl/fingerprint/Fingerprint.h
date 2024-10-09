79910ce0d146e0ad51757795ecce242fa0dea1cc
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-10-09 15:07:21 +0200

    zenfone8: fingerprint: Migrate our previous implementation to AIDL
    Co-authored-by: IPSBHANGU <inderpreetbhangu2@gmail.com>
    Change-Id: Ib410ef5d92747deb9af34b3eb88f620c75797618
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- aidl/fingerprint/Fingerprint.h
+++ aidl/fingerprint/Fingerprint.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2024 The LineageOS Project
+ *               2024 Paranoid Android
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,18 +9,51 @@
 
 #include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 
+#include "FingerprintConfig.h"
+#include "LockoutTracker.h"
+#include "Session.h"
+
+#include <vendor/goodix/hardware/biometrics/fingerprint/2.1/IGoodixFingerprintDaemon.h>
+
+using ::aidl::android::hardware::biometrics::fingerprint::FingerprintSensorType;
 using ::aidl::android::hardware::biometrics::fingerprint::ISession;
 using ::aidl::android::hardware::biometrics::fingerprint::ISessionCallback;
 using ::aidl::android::hardware::biometrics::fingerprint::SensorProps;
+
+using ::vendor::goodix::hardware::biometrics::fingerprint::V2_1::IGoodixFingerprintDaemon;
 
 namespace aidl::android::hardware::biometrics::fingerprint {
 
 class Fingerprint : public BnFingerprint {
   public:
+    Fingerprint();
+    ~Fingerprint();
+
     ndk::ScopedAStatus getSensorProps(std::vector<SensorProps>* _aidl_return) override;
     ndk::ScopedAStatus createSession(int32_t sensorId, int32_t userId,
                                      const std::shared_ptr<ISessionCallback>& cb,
                                      std::shared_ptr<ISession>* out) override;
+
+    static FingerprintConfig& cfg() {
+        static FingerprintConfig* cfg = nullptr;
+        if (cfg == nullptr) {
+            cfg = new FingerprintConfig();
+            cfg->init();
+        }
+        return *cfg;
+    }
+
+  private:
+    static fingerprint_device_t* openSensorHal(const char* class_name);
+    static SensorLocation getSensorLocation();
+    static void notify(const fingerprint_msg_t* msg);
+
+    std::shared_ptr<Session> mSession;
+    LockoutTracker mLockoutTracker;
+    FingerprintSensorType mSensorType;
+
+    fingerprint_device_t* mDevice;
+    ::android::sp<IGoodixFingerprintDaemon> mGoodixFingerprintDaemon;
 };
 
 }  // namespace aidl::android::hardware::biometrics::fingerprint
