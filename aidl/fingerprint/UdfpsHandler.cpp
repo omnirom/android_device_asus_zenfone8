7685783b336a465e4d2de14fc778547b9560e863
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-10-09 15:07:21 +0200

    zenfone8: fingerprint: Migrate our previous implementation to AIDL
    Co-authored-by: IPSBHANGU <inderpreetbhangu2@gmail.com>
    Change-Id: Ib410ef5d92747deb9af34b3eb88f620c75797618
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

---
+++ aidl/fingerprint/UdfpsHandler.cpp
@@ -0,0 +1,116 @@
+
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHander"
+
+#include "UdfpsHandler.h"
+
+#include <android-base/logging.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <thread>
+#include <unistd.h>
+#include <vendor/goodix/hardware/biometrics/fingerprint/2.1/IGoodixFingerprintDaemon.h>
+
+using ::android::sp;
+using ::android::hardware::hidl_vec;
+using ::vendor::goodix::hardware::biometrics::fingerprint::V2_1::IGoodixFingerprintDaemon;
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class UdfpsHandlerSake : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t *device) {
+        mDevice = device;
+
+        mGoodixFingerprintDaemon = IGoodixFingerprintDaemon::getService();
+
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+
+                mGoodixFingerprintDaemon->sendCommand(readBool(fd) ? CMD_LIGHT_AREA_STABLE
+                    : CMD_LIGHT_AREA_CLOSE, {}, [](int, const hidl_vec<signed char>&) {});
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_DOWN, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+    }
+
+    void onFingerUp() {
+        mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_UP, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+    }
+
+    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+        // nothing
+    }
+
+    void cancel() {
+        // nothing
+    }
+
+    void preEnroll() {
+        // nothing
+    }
+
+    void enroll() {
+        // nothing
+    }
+
+    void postEnroll() {
+        // nothing
+    }
+
+  private:
+    fingerprint_device_t *mDevice;
+    sp<IGoodixFingerprintDaemon> mGoodixFingerprintDaemon;
+};
+
+static UdfpsHandler* create() {
+    return new UdfpsHandlerSake();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
---
+++ aidl/fingerprint/UdfpsHandler.h
@@ -0,0 +1,49 @@
+
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <hardware/fingerprint.h>
+#include <stdint.h>
+
+#define AOD_ENABLED "doze_always_on"
+#define AOD_PROPERTY "vendor.asus.touch_control_aod"
+#define AOD_PROPERTY_FALSE "0"
+#define AOD_PROPERTY_TRUE "1"
+
+#define CMD_FINGER_DOWN 200001
+#define CMD_FINGER_UP 200003
+#define CMD_LIGHT_AREA_CLOSE 200000
+#define CMD_LIGHT_AREA_STABLE 200002
+#define CMD_PARTIAL_FINGER_DETECTED 200004
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+#define UDFPS_PROPERTY "persist.vendor.asus.fp.wakeup"
+#define UDFPS_PROPERTY_FALSE "false"
+#define UDFPS_PROPERTY_TRUE "true"
+
+class UdfpsHandler {
+  public:
+    virtual ~UdfpsHandler() = default;
+
+    virtual void init(fingerprint_device_t* device) = 0;
+    virtual void onFingerDown(uint32_t x, uint32_t y, float minor, float major) = 0;
+    virtual void onFingerUp() = 0;
+
+    virtual void onAcquired(int32_t result, int32_t vendorCode) = 0;
+    virtual void cancel() = 0;
+
+    virtual void preEnroll() = 0;
+    virtual void enroll() = 0;
+    virtual void postEnroll() = 0;
+};
+
+struct UdfpsHandlerFactory {
+    UdfpsHandler* (*create)();
+    void (*destroy)(UdfpsHandler* handler);
+};
