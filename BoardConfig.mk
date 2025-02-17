ec4fba3f63a462195014967cefcaec9d2bfe9228
LuK1337 <priv.luk@gmail.com>
2025-02-17 23:07:57 +0100

    zenfone8: Migrate to avbtool --set_verification_disabled_flag
    Change-Id: Ib73f4d97a923b25e25848b24a3554cdd4de1a5ae
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ TARGET_NO_BOOTLOADER := true
 # A/B
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_verification_disabled_flag
 BOARD_AVB_VBMETA_SYSTEM := product system system_ext
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA4096
