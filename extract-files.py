0776a950f8b44025d0f61a26fe5b17fa8eff9daa
Tuan Anh <tuan73176@gmail.com>
2025-03-23 13:52:48 +0700

    zenfone8: Patch libwvdrmengine to load libcrypto_shim
    It turns out, this one also relies on CBS symbols.
    
    Change-Id: Ie54805de81eaef58cdf891e1e6df4579afec6e06
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- extract-files.py
+++ extract-files.py
@@ -61,7 +61,9 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/nfc_nci_nxp_prebuilt.so'): blob_fixup()
         .replace_needed('ese_spi_nxp.so', 'ese_spi_nxp_prebuilt.so')
         .add_needed('libbase_shim.so'),
-    'vendor/lib64/libwvhidl.so': blob_fixup()
+    ('vendor/lib/mediadrm/libwvdrmengine.so',
+     'vendor/lib64/mediadrm/libwvdrmengine.so',
+    'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     ('vendor/lib/ese_spi_nxp_prebuilt.so',
      'vendor/lib64/ese_spi_nxp_prebuilt.so'): blob_fixup()
