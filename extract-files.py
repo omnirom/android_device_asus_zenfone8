5003aebfe40f61f4855544eeb980db07c3c1f6ab
micky387 <mickaelsaibi@free.fr>
2025-06-27 19:41:28 -0400

    zenfone8: Add fm libs and deps
    Change-Id: I0e61393c8254174c3132f70ef99b00d132b35f20
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
         'libxditk_DIT_Manager',
         'libxditk_ISP',
