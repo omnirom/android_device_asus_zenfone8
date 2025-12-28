c74b135016f6f8c4ce2beeffcc0eaa820e9e0ff5
micky387 <mickaelsaibi@free.fr>
2025-12-27 20:27:58 -0500

    zenfone8: Imports sm8350 qcom-caf to namespace
    Change-Id: If3e7817757de157fcf0a59087289028af478d9f1
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/sm8350',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
